258f8753f7116ae8a110dab954984e834bba89e7
Timothy Roberts <timber110406@gmail.com>
2025-09-09 08:10:12 -0700

    added analytics

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/footer/footer";
-
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,6 +26,7 @@ export default function RootLayout({ children }) {
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <Navbar />
         {children}
+        <Analytics />
         <Footer />
       </body>
     </html>
